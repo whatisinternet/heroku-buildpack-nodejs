c62482cd9dd2a0330eb771b92be4d79af1b830b2
Whatisinternet <joshteeter@gmail.com>
2016-08-17 17:24:21 -0400

    Wire up

--- lib/environment.sh
+++ lib/environment.sh
@@ -1,8 +1,8 @@
 create_default_env() {
-  export NPM_CONFIG_PRODUCTION=${NPM_CONFIG_PRODUCTION:-true}
+  export NPM_CONFIG_PRODUCTION=${NPM_CONFIG_PRODUCTION:-false}
   export NPM_CONFIG_LOGLEVEL=${NPM_CONFIG_LOGLEVEL:-error}
   export NODE_MODULES_CACHE=${NODE_MODULES_CACHE:-true}
-  export NODE_ENV=${NODE_ENV:-production}
+  export NODE_ENV=${NODE_ENV:-development}
 }
 
 list_node_config() {
